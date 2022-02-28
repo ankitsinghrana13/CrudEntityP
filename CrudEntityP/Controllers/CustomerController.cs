e5908c2b5df15e23d80dbe4fd539ccf84a4f9119
ankitsinghrana13 <ak9897432538@gmail.com>
2022-03-01 02:41:22 +0800

    Add project files.

---
+++ CrudEntityP/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using CrudEntityP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudEntityP.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext dbContext;
+        public CustomerController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Location> Locations = dbContext.Locations.ToList();
+            return View(Locations);
+        }
+        public IActionResult CustomerList(int id)
+        {
+            List<Customer> Customers = dbContext.Customers.Where(e => e.Location.Id == id).ToList();
+            return View(Customers);
+        }
+    }
+}
